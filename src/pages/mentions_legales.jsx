4ab2d222c28ec590f4e2dda20b768d4269a89356
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-05-20 16:03:08 +0200

    padding right p

--- src/pages/mentions_legales.jsx
+++ src/pages/mentions_legales.jsx
@@ -15,10 +15,10 @@ function Mentions_Legales() {
                 </ul>
 
                 <h2 className="pl-[10px] mt-[40px] mx-[0px] mb-[10px] text-left text-base lg:text-3xl">Propriété intellectuelle:</h2>
-                <p className="text-xs m-[0px] mr-auto pl-[2rem] lg:text-xl">Le contenu de ce site (textes, images, code, etc.) est la propriété de Tom Fourneaux et est protégé par le droit d’auteur. Toute reproduction ou représentation sans autorisation préalable est interdite.</p>
+                <p className="text-xs pr-[2rem] mr-auto pl-[2rem] lg:text-xl">Le contenu de ce site (textes, images, code, etc.) est la propriété de Tom Fourneaux et est protégé par le droit d’auteur. Toute reproduction ou représentation sans autorisation préalable est interdite.</p>
 
                 <h2 className="pl-[10px] mt-[40px] mx-[0px] mb-[10px] text-left text-base lg:text-3xl">Protection des données personnelles:</h2>
-                <p className="text-xs m-[0px] mr-auto pl-[2rem] lg:text-xl">Aucune donnée personnelle {"n'est"} collectée sur ce site, sauf si vous conctactez Tom Fourneaux grâce à la page <Link to="/me_contacter" className="text-white">contact</Link>. Les informations fournies ne seront utilisées que dans le cadre de la réponse à vos demandes. Vous avez un droit {"d'accès"}, de rectification et de suppression sur vos données personnelles.
+                <p className="text-xs pr-[2rem] mr-auto pl-[2rem] lg:text-xl">Aucune donnée personnelle {"n'est"} collectée sur ce site, sauf si vous conctactez Tom Fourneaux grâce à la page <Link to="/me_contacter" className="text-white">contact</Link>. Les informations fournies ne seront utilisées que dans le cadre de la réponse à vos demandes. Vous avez un droit {"d'accès"}, de rectification et de suppression sur vos données personnelles.
                 </p>
         </>
     );
