a877f7b7e9913c0a9e44beda99a1bfa266ff62e5
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-05-20 15:53:19 +0200

    fix margin p mentions legales and clicker in error

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,9 +1,9 @@
-import NicoJr from "../assets/img/NicoJr.png"
+import NicoJr from "../assets/img/error-clicker/face_nico_jr.png"
 
 function Error() {
     return (
         <>
-        <div className="flex flex-col justify-center justify-items-center max-h-[600px] h-screen">
+        <div className="flex flex-col justify-center justify-items-center h-screen">
             <h1 className="text-xl flex justify-center lg:text-8xl">404</h1>
             <p className="text-sm flex justify-center lg:text-3xl">Page Not Found</p>
         </div>
--- src/pages/mentions_legales.jsx
+++ src/pages/mentions_legales.jsx
@@ -11,14 +11,14 @@ function Mentions_Legales() {
                     <li>- Nom: Tom Fourneaux</li>
                     <li>- Statut: Etudiant Bachelor chef de projets digitaux<br/>Normandie Web School, Rouen</li>
                     <li>- Email: tfourneaux@normandiewebschool.fr</li>
-                    <li>- Site web: <Link to="https://tomfrx12.github.io/tom-fourneaux-portfolio/" className="text-[var(--color-grey)] text-xs lg:text-2xl"><br/>https://tomfrx12.github.io/tom-fourneaux-portfolio/</Link></li>
+                    <li>- Site web: <Link to="https://tom-fourneaux.fr" className="text-[var(--color-grey)] text-xs lg:text-2xl"><br/>https://tomfrx12.github.io/tom-fourneaux-portfolio/</Link></li>
                 </ul>
 
                 <h2 className="pl-[10px] mt-[40px] mx-[0px] mb-[10px] text-left text-base lg:text-3xl">Propriété intellectuelle:</h2>
-                <p className="text-xs m-[0px] pl-[2rem] lg:text-xl">Le contenu de ce site (textes, images, code, etc.) est la propriété de Tom Fourneaux et est protégé par le droit d’auteur. Toute reproduction ou représentation sans autorisation préalable est interdite.</p>
+                <p className="text-xs m-[0px] mr-auto pl-[2rem] lg:text-xl">Le contenu de ce site (textes, images, code, etc.) est la propriété de Tom Fourneaux et est protégé par le droit d’auteur. Toute reproduction ou représentation sans autorisation préalable est interdite.</p>
 
                 <h2 className="pl-[10px] mt-[40px] mx-[0px] mb-[10px] text-left text-base lg:text-3xl">Protection des données personnelles:</h2>
-                <p className="text-xs m-[0px] pl-[2rem] lg:text-xl">Aucune donnée personnelle {"n'est"} collectée sur ce site, sauf si vous conctactez Tom Fourneaux grâce à la page <Link to="/me_contacter" className="text-white">contact</Link>. Les informations fournies ne seront utilisées que dans le cadre de la réponse à vos demandes. Vous avez un droit {"d'accès"}, de rectification et de suppression sur vos données personnelles.
+                <p className="text-xs m-[0px] mr-auto pl-[2rem] lg:text-xl">Aucune donnée personnelle {"n'est"} collectée sur ce site, sauf si vous conctactez Tom Fourneaux grâce à la page <Link to="/me_contacter" className="text-white">contact</Link>. Les informations fournies ne seront utilisées que dans le cadre de la réponse à vos demandes. Vous avez un droit {"d'accès"}, de rectification et de suppression sur vos données personnelles.
                 </p>
         </>
     );
