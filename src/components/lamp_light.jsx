38fd0b592c06f4a5c4c42d33add1f752ea2ffbf1
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-06-04 10:47:20 +0200

    fix

--- src/components/lamp_light.jsx
+++ src/components/lamp_light.jsx
@@ -5,24 +5,6 @@ import PropTypes from 'prop-types';
 
 const Lamp = ({ itsDark, setItsDark }) => {
     return (            
-        // {/* <img className="size-50 cursor-pointer" src={lamp} alt="Lamp of the Lamp" onClick={() => setItsDark(!itsDark)}/>
-        //     {itsDark && (
-        //         <img className="relative size-80" src={light} alt="Light enabled"/>
-        //     )} */}
-
-            // <div className="flex flex-col items-center">
-            //     <img src={lamp} alt="Test SVG Image" className="w-[300px] h-[300px] mb-[-25px]"/>
-            //     <img src={light} alt="Test SVG Image" className="w-[300px] h-[300px]"/>
-            // </div>            {/* <img className="size-50 cursor-pointer" src={lamp} alt="Lamp of the Lamp" onClick={() => setItsDark(!itsDark)}/>
-            // {itsDark && (
-            //     <img className="relative size-80" src={light} alt="Light enabled"/>
-            // )} */}
-
-            // <div className="flex flex-col items-center">
-            //     <img src={lamp} alt="Test SVG Image" className="w-[300px] h-[300px] mb-[-25px]"/>
-            //     <img src={light} alt="Test SVG Image" className="w-[300px] h-[300px]"/>
-            // </div>
-
         <div className="flex flex-col items-center">
             <img className="w-[300px] h-[300px] mb-[-25px] cursor-pointer" src={lamp} alt="Lamp of the Lamp" onClick={() => setItsDark(!itsDark)}/>
             {itsDark && (
