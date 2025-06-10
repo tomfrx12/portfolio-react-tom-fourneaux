0657ad7b006f9b26f6761b1dcd7c52ce1ba36283
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-06-10 19:47:28 +0200

    fix scroll and button seemore in projets

--- src/components/card_projet.jsx
+++ src/components/card_projet.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router';
 import {Text} from './texte';
 import PropTypes from "prop-types";
+import SeeMore from './see_more';
 
 export const CardProjet = ({id_project, img_illustr, alt_illustr, title_text, text, text_link, link, title_link, img_link, alt_link, className_img}) => {
     return (
         <div id={id_project} className="bg-[var(--color-grey)] rounded-[10px] m-[20px] flex flex-col lg: p-[20px] lg:items-center lg:justify-around lg:mb-[40px] lg:mt-[40px] shadow-[10px_10px_10px_black]">
             <img className={className_img} src={img_illustr} alt={alt_illustr}/>
-            {/* m-[30px] max-w-full max-h-max text-center */}
             <div className="flex flex-col lg:w-[50%] lg:justify-center">
                 <div>
                     <Text tag="h2" name="h2" className="font-black">
@@ -16,10 +16,11 @@ export const CardProjet = ({id_project, img_illustr, alt_illustr, title_text, te
                         {text}
                     </Text>
                 </div>
-                <Text tag="p" className="mb-[0px] ml-[10px]">
+                {/* <Text tag="p" className="mb-[0px] ml-[10px]">
                     {text_link}
-                </Text>
-                <Link to={link} title={title_link} className="max-w-[40px] max-h-[40px] bg-white text-black rounded-[20px] p-[5px] my-[5px] text-center self-center transition-transform duration-300 ease-in-out shadow-[0px_0px_50px_black] hover:scale-105 lg:flex lg:justify-center lg:hover:scale-115">
+                </Text> */}
+                <SeeMore link={text_link} onClick={() => window.scrollTo(0, 0)}/>
+                <Link to={link} title={title_link} onClick={() => window.scrollTo(0, 0)} className="max-w-[40px] max-h-[40px] bg-white text-black rounded-[20px] p-[5px] my-[5px] text-center self-center transition-transform duration-300 ease-in-out shadow-[0px_0px_50px_black] hover:scale-105 lg:flex lg:justify-center lg:hover:scale-115">
                     <img className="h-[25px] max-w-full" src={img_link} alt={alt_link}/>
                 </Link>
             </div>
