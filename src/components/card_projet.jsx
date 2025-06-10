bf0ad29772703a5d56e2920b286ba7e7b479514b
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-06-10 20:20:36 +0200

    fix link into card projet

--- src/components/card_projet.jsx
+++ src/components/card_projet.jsx
@@ -3,7 +3,7 @@ import {Text} from './texte';
 import PropTypes from "prop-types";
 import SeeMore from './see_more';
 
-export const CardProjet = ({id_project, img_illustr, alt_illustr, title_text, text, text_link, link, title_link, img_link, alt_link, className_img}) => {
+export const CardProjet = ({id_project, img_illustr, alt_illustr, title_text, text, link, title_link, img_link, alt_link, className_img}) => {
     return (
         <div id={id_project} className="bg-[var(--color-grey)] rounded-[10px] m-[20px] flex flex-col lg: p-[20px] lg:items-center lg:justify-around lg:mb-[40px] lg:mt-[40px] shadow-[10px_10px_10px_black]">
             <img className={className_img} src={img_illustr} alt={alt_illustr}/>
@@ -19,7 +19,7 @@ export const CardProjet = ({id_project, img_illustr, alt_illustr, title_text, te
                 {/* <Text tag="p" className="mb-[0px] ml-[10px]">
                     {text_link}
                 </Text> */}
-                <SeeMore link={text_link} onClick={() => window.scrollTo(0, 0)}/>
+                <SeeMore link={link} onClick={() => window.scrollTo(0, 0)}/>
                 <Link to={link} title={title_link} onClick={() => window.scrollTo(0, 0)} className="max-w-[40px] max-h-[40px] bg-white text-black rounded-[20px] p-[5px] my-[5px] text-center self-center transition-transform duration-300 ease-in-out shadow-[0px_0px_50px_black] hover:scale-105 lg:flex lg:justify-center lg:hover:scale-115">
                     <img className="h-[25px] max-w-full" src={img_link} alt={alt_link}/>
                 </Link>
@@ -34,7 +34,6 @@ CardProjet.propTypes = {
     alt_illustr: PropTypes.node,
     title_text: PropTypes.node,
     text: PropTypes.node,
-    text_link: PropTypes.node,
     link: PropTypes.node,
     title_link: PropTypes.node,
     img_link: PropTypes.node,
