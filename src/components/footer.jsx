e2cc536cc5706155bee812f8b4f18571797254c5
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-09-07 20:56:07 +0200

    idk

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ import instagram from "../assets/img/img_reseaux/instagram.png"
 
 const Footer = () => {
     return (
-        <footer className="mt-[20px] bg-black flex justify-center flex-col items-center lg:flex-row lg:justify-around lg:py-[10px]">
+        <footer className="bg-black flex justify-center flex-col items-center lg:flex-row lg:justify-around lg:py-[10px]">
             <nav className="flex gap-[40px] my-[20px] lg:m-[0]">
                 <Link to="https://github.com/tomfrx12" className="no-underline transition-transform duration-300 ease-in-out transform hover:scale-120">
                     <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src={github} alt="Logo de Github"/>
