3c3e91fc67e965e74c5fab438db96badebc11777
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-11-10 14:36:53 +0100

    fix img

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,7 +27,7 @@ function Index({ itsDark, setItsDark }) { //le bool et la func qui est définie 
             <section className="flex flex-col h-[95vh] lg:flex-row">
                 <div className="flex items-center justify-center flex-col lg:w-[40%] bg-(--color-white-background) dark:bg-(--color-white-background-dark)">
                     <button onClick={Clicker}>
-                        <img src="/src/assets/img/A2_Fourneaux_Tom.png" alt="photo de profil Tom FOURNEAUX" className="my-5 m-auto rounded-full"/>
+                        <img src="src/assets/img/A2_Fourneaux_Tom.png" alt="photo de profil Tom FOURNEAUX" className="my-5 m-auto rounded-full"/>
                     </button>
                     {ButtonClicker && (
                         <Text tag="p" size="base">Nombre de click : {NbClick}</Text>
@@ -56,7 +56,7 @@ function Index({ itsDark, setItsDark }) { //le bool et la func qui est définie 
                             <PresentationIndex>Je suis un jeune développeur web de 18 ans, actuellement étudiant à la Normandie Web School (NWS) de Rouen, où je suis inscrit en Bachelor Chef de Projets Digitaux. Passionné par le monde du développement web depuis mon plus jeune âge, j’ai naturellement orienté mes études et ma carrière vers ce domaine en constante évolution. Ce qui me motive avant tout, c’est la possibilité de créer des expériences numériques qui répondent aux besoins des utilisateurs tout en étant techniquement performantes et esthétiquement soignées.</PresentationIndex>
                             <PresentationIndex>Ma formation m’a permis de me familiariser avec plusieurs technologies essentielles, notamment HTML, CSS, et JavaScript, des langages que j’utilise au quotidien pour la création et l’optimisation de sites web. En parallèle, je me forme également à des frameworks modernes comme React, afin d’élargir mes compétences et d’être capable de réaliser des projets web plus complexes et dynamiques. Cette diversité de technologies me permet de m’adapter aux différents besoins des projets et de toujours offrir des solutions efficaces et innovantes.</PresentationIndex>
                         </div>
-                        <img src="/src/assets/img/illustration presentation/illustration dev 1.png" alt="illustration developpeur web" className="m-auto max-w-[90%] p-[15px] lg:self-center lg:w-auto lg:max-w-[35%]"/>
+                        <img src="src/assets/img/illustration presentation/illustration dev 1.png" alt="illustration developpeur web" className="m-auto max-w-[90%] p-[15px] lg:self-center lg:w-auto lg:max-w-[35%]"/>
                         <div>
                             <PresentationIndex>Au-delà de la technique, mon cursus à la NWS m’a aussi permis de me perfectionner dans des compétences transversales telles que la gestion de projet et la collaboration au sein d’équipes pluridisciplinaires. Je suis convaincu que la réussite d’un projet web ne réside pas seulement dans la qualité du code, mais aussi dans la capacité à communiquer, à organiser le travail et à respecter les délais tout en répondant aux attentes du client. C’est pourquoi je {"m'efforce d’allier"} expertise technique et sens de l’organisation pour garantir la réussite des projets auxquels je participe.</PresentationIndex>
                             <PresentationIndex>Aujourd’hui, je suis à la recherche d’un stage ou d’une alternance en développement web, afin de mettre en pratique mes connaissances et de continuer à apprendre au contact de professionnels expérimentés. Je suis particulièrement intéressé par les environnements de travail collaboratifs, où je pourrais à la fois apporter mes compétences et en acquérir de nouvelles. Vous pouvez consulter mon CV pour plus de détails sur mes compétences et expériences.</PresentationIndex>
@@ -81,7 +81,7 @@ function Index({ itsDark, setItsDark }) { //le bool et la func qui est définie 
                     </div>
 
                     <div className="flex items-center my-5">
-                        <img src="/src/assets/img/logo_des_projets/logo-club-eco-st-sever.png" alt="Club Eco Saint Sever" className="max-h-20 my-2.5 mx-5"/>
+                        <img src="src/assets/img/logo_des_projets/logo-club-eco-st-sever.png" alt="Club Eco Saint Sever" className="max-h-20 my-2.5 mx-5"/>
                         <div>
                             <Text tag="h3" name="h3" size="lg">Club Eco Saint Sever</Text>
                             <Text tag="p" size="base">Projet qui consistait à créer une identité visuelle pour le Club Eco de Saint Sever.</Text>
