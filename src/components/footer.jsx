c69be843dde117a3201da91090a0c72152db596b
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-11-10 17:00:23 +0100

    fix img footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router";
 
-import github from "../assets/img/img_reseaux/github.png"
-import linkedin from "../assets/img/img_reseaux/linkedin.png"
-import instagram from "../assets/img/img_reseaux/instagram.png"
+import github from "/public/img/img_reseaux/github.png"
+import linkedin from "/public/img/img_reseaux/linkedin.png"
+import instagram from "/public/img/img_reseaux/instagram.png"
 
 const Footer = () => {
     return (
         <footer className="bg-black flex justify-center flex-col items-center lg:flex-row lg:justify-around lg:py-[10px]">
             <nav className="flex gap-[40px] my-[20px] lg:m-[0]">
                 <Link to="https://github.com/tomfrx12" className="no-underline transition-transform duration-300 ease-in-out transform hover:scale-120">
-                    <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src={github} alt="Logo de Github"/>
+                    <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src="/public/img/img_reseaux/github.png" alt="Logo de Github"/>
                 </Link>
                 <Link to="https://www.linkedin.com/in/tom-fourneaux-078644332/" className="no-underline transition-transform duration-300 ease-in-out transform hover:scale-120">
-                    <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src={linkedin} alt="Logo de LinkedIn"/>
+                    <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src="/public/img/img_reseaux/linkedin.png" alt="Logo de LinkedIn"/>
                 </Link>
                 <Link to="https://www.instagram.com/fourneauxtom/" className="no-underline transition-transform duration-300 ease-in-out transform hover:scale-120">
-                    <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src={instagram} alt="Logo de Instagram"/>
+                    <img className="max-h-[1.5rem] lg:max-h-[2.5rem]" src="/public/img/img_reseaux/instagram.png" alt="Logo de Instagram"/>
                 </Link>
             </nav>
             <p className="text-center text-xs text-white lg:text-2xl">Copyright © 2025 tom-fourneaux<br/>
