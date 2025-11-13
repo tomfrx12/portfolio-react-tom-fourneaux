6879fff55d7e1f376b4689fd317e0f67059a9a82
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-11-13 11:39:59 +0100

    presentation_index

--- src/components/presentation_index.jsx
+++ src/components/presentation_index.jsx
@@ -1,14 +1,9 @@
 import { Text } from "./texte";
-import PropTypes from "prop-types";
 
 export const PresentationIndex = ({children}) => {
     return (
-        <Text tag="p" size="base" className="border-l-5 border-l-[#ff6347] m-[0px 0px 20px 20px] px-[15px] mt-[2.5rem]">
+        <Text tag="p" size="base" className="border-l-5 border-l-[#ff6347] m-[0px 0px 20px 20px] px-[15px] mt-10">
             {children}
         </Text>
     );
 }
-
-PresentationIndex.propTypes = {
-  children: PropTypes.node,
-};
